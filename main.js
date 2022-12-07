a5d912693b5a41e709278549639d2cffc5f2a665
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-12-06 21:13:23 -0800

    cursor javascript

---
+++ main.js
@@ -0,0 +1,15 @@
+const mouseCircle = document.querySelector(".mouse-circle");
+const mouseDot = document.querySelector(".mouse-dot");
+
+// mouse circle
+
+const mouseCircleFn= (x,y)=>{
+    mouseCircle.style.cssText ="top:${y}px; left:${x}px";
+}
+
+
+document.body.addEventListener('mousemove',(e)=>{
+    let x = e.clientX;
+    let y = e. clientY;
+    mouseCircleFn(x,y);
+})
