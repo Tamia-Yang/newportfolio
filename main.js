ff88704263c5cd69ab0dde4af5a49406d02bb2e4
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-12-06 21:31:05 -0800

    modify total

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ const mouseCircleFn= (x,y)=>{
     mouseCircle.style.cssText ="top:${y}px; left:${x}px";
 }
 
-
 document.body.addEventListener('mousemove',(e)=>{
     let x = e.clientX;
     let y = e. clientY;
     mouseCircleFn(x,y);
 })
+
+// end of mouse circle
