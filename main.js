c1573a261760b9915952306a1c4574fd3da30e49
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-12-07 13:09:18 -0800

    image effect

--- main.js
+++ main.js
@@ -4,14 +4,22 @@ const mouseDot = document.querySelector(".mouse-dot");
 // mouse circle
 
 const mouseCircleFn= (x,y)=>{
-    mouseCircle.style.cssText =`top:${y}px; left:${x}px`;
-    mouseDot.style.cssText =`top:${y}px; left:${x}px`;
+    mouseCircle.style.cssText =`top:${y}px; left:${x}px; opacity:1`;
+    mouseDot.style.cssText =`top:${y}px; left:${x}px; opacity:1`;
+    
 }
 
 document.body.addEventListener('mousemove',(e)=>{
 
     let x = e.clientX;
     let y = e.clientY;
+   
+    mouseCircleFn(x,y);
+})
+document.body.addEventListener('mouseleave',(e)=>{
+
+    mouseCircle.style.opacity="0";
+    mouseDot.style.opacity="0";
    
     mouseCircleFn(x,y);
 })
