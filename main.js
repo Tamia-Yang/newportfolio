6f5244dc1a0f9bd208e9711935c42559dd6c8994
Tamia-Yang <76608164+Tamia-Yang@users.noreply.github.com>
2022-12-09 16:46:49 -0800

    landing page animation

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
+const circles = document.querySelectorAll(".circle");
+const mainimg = document.querySelector("img");
+
+
+let prevX = 0, prevY = 0; // previeous Coordinates
+window.addEventListener("mousemove",(e,x,y)=>{
+   if(prevX < e.clientX){
+    circles.forEach(circle =>{
+        circle.style.left =`150px`;
+    })
+
+   }
+   prevX = e.clientX;
+   prevY = e.clientY;
+})
+
 
 // mouse circle
 
-const mouseCircleFn= (x,y)=>{
-    mouseCircle.style.cssText =`top:${y}px; left:${x}px; opacity:1`;
-    mouseDot.style.cssText =`top:${y}px; left:${x}px; opacity:1`;
+// const mouseCircleFn= (x,y)=>{
+//     mouseCircle.style.cssText =`top:${y}px; left:${x}px; opacity:1`;
+//     mouseDot.style.cssText =`top:${y}px; left:${x}px; opacity:1`;
     
-}
+// }
 
-document.body.addEventListener('mousemove',(e)=>{
+// document.body.addEventListener('mousemove',(e)=>{
 
-    let x = e.clientX;
-    let y = e.clientY;
+//     let x = e.clientX;
+//     let y = e.clientY;
+
+//     mouseCircleFn(x,y);
+// })
+// document.body.addEventListener('mouseleave',(e)=>{
+
+//     mouseCircle.style.opacity="0";
+//     mouseDot.style.opacity="0";
    
-    mouseCircleFn(x,y);
-})
-document.body.addEventListener('mouseleave',(e)=>{
-
-    mouseCircle.style.opacity="0";
-    mouseDot.style.opacity="0";
-   
-    mouseCircleFn(x,y);
-})
+//     mouseCircleFn(x,y);
+// })
 
 // end of mouse circle
+
